501fed5cda1c7f7b94a0bce1d9ec5077bc6a1794
Isabel Frade <33909806+Ifrade1@users.noreply.github.com>
2020-09-22 23:43:03 -0400

    now removes space

--- rnaFoldrm.pl
+++ rnaFoldrm.pl
@@ -1,28 +1,34 @@
+
 my $RNA_FOLD = $ARGV[0];
 
 open (FILE_TO_READ, "<", "$RNA_FOLD")
   or die "Cannot open file $RNA_FOLD : $!";
 my $RNA;
+my $rnaName = "";
 while (my $line = <FILE_TO_READ>) {
   if ($line !~ /^>/) {
-    chomp($line);
+     chomp($line);
     $RNA = $RNA.$line;
   }
-  elsif ($line !~/^@/){
+  if ($line =~ /^>/) {
     chomp($line);
     $rnaName = $rnaName.$line;
   }
+
 }
 close (FILE_TO_READ);
 
-$RNA =~ tr/ACGU//d;
+$RNA =~ tr/ACGUT//d;
 $rnaName =~ tr/>//d;
-$rnaName =~ tr/, /_/;
-$filename = "$rnaName.txt";
+my $len = length($RNA);
+my $loc = index($RNA, " ");
+print $loc;
+my $RNA = substr($RNA, 0, $loc);
 
-open (FILE_TO_WRITE, ">", "$filename")
+open (FILE_TO_WRITE, ">", "$rnaName")
      or die "Cannot open $filename to write: $!";
-print FILE_TO_WRITE "$RNA";
+print FILE_TO_WRITE "$RNA\n";
+print FILE_TO_WRITE "$RNA2";
 close(FILE_TO_WRITE);
 if (-e $filename){
   print "RNA were written to $filename file successfully!\n";
