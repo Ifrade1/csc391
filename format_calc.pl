2d86160b939821ec98e185a7cb44deecc89c763f
Isabel Frade <33909806+Ifrade1@users.noreply.github.com>
2020-10-06 23:14:26 -0400

    Update format_calc.pl

--- format_calc.pl
+++ format_calc.pl
@@ -62,16 +62,17 @@ for (my $i = 0; $i < (scalar @diffs); $i++) {
   print $diffs[$i].", ";			#print curr element of @diffs
   $count = $count + $diffs[$i];			#add $count to curr element
 } print "\n";
-
 my $avg = $count / scalar @diffs;		#average the values
 print "Avg: $avg"."\n";				#print average
-my $med;
-my @medArray = sort @diffs;
+
+#find median, for for stats info
+my $med = 0.0;
+my $diffSize = @diffs;
+my @medArray = sort{$a <=> $b} @diffs;
 if (@medArray % 2 == 0){
 $med = ($medArray[(@medArray/2)-1] + $medArray[(@medArray/2)])/2;
-print "even\n";
 }
 else{
 $med = $medArray[@medArray/2];
         }
-print "Med: $med"."\n";
+printf("Med: %.1f \n", $med);
