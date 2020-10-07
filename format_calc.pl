d2216a5070a7b1db3b312ac0c6ffe4cae1a4d872
Isabel Frade <33909806+Ifrade1@users.noreply.github.com>
2020-10-06 20:51:11 -0400

    Update format_calc.pl

--- format_calc.pl
+++ format_calc.pl
@@ -1,67 +1,33 @@
-#use strict;
-#!/usr/bin/perl -w
-
-#Takes in the file, removes spaces between lines 
-my $RNA_FOLD = $ARGV[0];
-open (FILE_TO_READ, "<", "$RNA_FOLD")
-  or die "Cannot open file $RNA_FOLD : $!";
-my $RNA;
-my $rnaName = "";
-while (my $line = <FILE_TO_READ>) {
-  if ($line !~ /^>/) {
-     chomp($line);
-    $RNA = $RNA.$line;
-  }
-  if ($line =~ /^>/) {
-    chomp($line);
-    $rnaName = $rnaName.$line;
-  }
-
-}
-close (FILE_TO_READ);
-
-#The file has the RNA sequence at the begining. that needs to be removed.
-$RNA =~ tr/ACGUT//d;
-$rnaName =~ tr/>//d;
-#Gets rid of the energy value at the end
-my $len = length($RNA);
-my $loc = index($RNA, " ");
-
-my $RNA = substr($RNA, 0, $loc);
-if ($rnaName eq ""){
-        $rnaName = "v".$RNA_FOLD;
-}
-#sends the vienna format to a file
-open (FILE_TO_WRITE, ">", "$rnaName")
-     or die "Cannot open $rnaName to write: $!";
-print FILE_TO_WRITE "$RNA\n";
 close(FILE_TO_WRITE);
 if (-e $rnaName){
   print "RNA were written to $rnaName file successfully!\n";
-}
-
+}                                                                                                                                           
+                                                                                                                                            
 my $s = $RNA;
-my @stack = ();					#holds last known ( index
-my @diffs = ();					#holds the distance between each () pair
-my $curr;					#holds each character of $s per loop
-my $dif;					#difference between ) - (
+#print "$s\n";
+my @stack = ();                                 #holds last known ( index
+my @diffs = ();                                 #holds the distance between each () pair
+my $curr;                                       #holds each character of $s per loop
+my $dif;                                        #difference between ) - (
 for (my $i = 0; $i < length($s); $i++) {
   $curr = substr($s, $i, 1);
-  if ($curr =~ /\(/) {				#if curr char matches (
+  if ($curr =~ /\(/) {                          #if curr char matches (
     push(@stack, $i);
-  }
-  elsif ($curr =~ /\)/) {			#if curr char matches )
-    $dif = ($i - pop(@stack));			#set dif
-    push(@diffs, $dif);				#append @diffs
-  }
+  }                                                                                                                                         
+  elsif ($curr =~ /\)/) {                       #if curr char matches )
+    $dif = ($i - pop(@stack));                  #set dif
+    push(@diffs, $dif);                         #append @diffs
+  }                                                                                                                                         
 }
-
-my $count = 0;					#adds all difs together
+                                                                                                                                            
+my $count = 0;                                  #adds all difs together
 print "Diffs: ";
-for (my $i = 0; $i < (scalar @diffs); $i++) {	
-  print $diffs[$i].", ";			#print curr element of @diffs
-  $count = $count + $diffs[$i];			#add $count to curr element
+for (my $i = 0; $i < (scalar @diffs); $i++) {
+  print $diffs[$i].", ";                        #print curr element of @diffs
+  $count = $count + $diffs[$i];                 #add $count to curr element
 } print "\n";
-
-my $avg = $count / scalar @diffs;		#average the values
-print "Avg: $avg"."\n";				#print average
+                                                                                                                                            
+my $avg = $count / scalar @diffs;               #average the values
+print "Avg: $avg"."\n";                         #print average
+                                                                                                                          68,1          Bot
+p
